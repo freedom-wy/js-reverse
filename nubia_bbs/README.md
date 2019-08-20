9e873f89df902c7f52f2ac3c89e0f2cb1876ea2d
dazhuang <1780292537@qq.com>
2019-08-20 21:01:24 +0800

    努比亚js破解

--- nubia_bbs/README.md
+++ nubia_bbs/README.md
@@ -6,7 +6,7 @@
 ##### handle_nubia_bbs.py 努比亚论坛cookie值破解python程序
 ***
 #### 逆向方法:
-##### 通过fiddler抓包发现第一次请求https://bbs.nubia.cn/时会返回一段JS代码
+##### 通过fiddler抓包发现第一次请求 https://bbs.nubia.cn/ 时会返回一段JS代码
 ##### 通过chrome浏览器调试，发现有debugger反调试,保存fiddler抓取到的js代码到本地
 ##### 通过js美化，格式化代码[美化JS代码](https://tool.lu/js/)
 ##### 通过chrome浏览器打开本地文件，在debugger情况下找到调用debugger的地方，删除代码,解除反debug
---
+++ nubia_bbs/overwrite_js.py
@@ -0,0 +1,21 @@
+"""
+掉头发！！！
+"""
+arg1 = "8C860D6CBDD7C96DC9EBEA5BF100595CCCDC6B22"
+_0x4818 = ['csKHwqMI', 'ZsKJwr8VeAsy', 'UcKiN8O/wplwMA==', 'JR8CTg==', 'YsOnbSEQw7ozwqZKesKUw7kwX8ORIQ==', 'w7oVS8OSwoPCl3jChMKhw6HDlsKXw4s/YsOG', 'fwVmI1AtwplaY8Otw5cNfSgpw6M=', 'OcONwrjCqsKxTGTChsOjEWE8PcOcJ8K6', 'U8K5LcOtwpV0EMOkw47DrMOX', 'HMO2woHCiMK9SlXClcOoC1k=', 'asKIwqMDdgMuPsOKBMKcwrrCtkLDrMKBw64d', 'wqImMT0tw6RNw5k=', 'DMKcU0JmUwUv', 'VjHDlMOHVcONX3fDicKJHQ==', 'wqhBH8Knw4TDhSDDgMOdwrjCncOWwphhN8KCGcKqw6dHAU5+wrg2JcKaw4IEJcOcwrRJwoZ0wqF9YgAV', 'dzd2w5bDm3jDpsK3wpY=', 'w4PDgcKXwo3CkcKLwr5qwrY=', 'wrJOTcOQWMOg', 'wqTDvcOjw447wr4=', 'w5XDqsKhMF1/', 'wrAyHsOfwppc', 'J3dVPcOxLg==', 'wrdHw7p9Zw==', 'w4rDo8KmNEw=', 'IMKAUkBt', 'w6bDrcKQwpVHwpNQwqU=', 'd8OsWhAUw7YzwrU=', 'wqnCksOeezrDhw==', 'UsKnIMKWV8K/', 'w4zDocK8NUZv', 'c8OxZhAJw6skwqJj', 'PcKIw4nCkkVb', 'KHgodMO2VQ==', 'wpsmwqvDnGFq', 'wqLDt8Okw4c=', 'w7w1w4PCpsO4wqA=', 'wq9FRsOqWMOq', 'byBhw7rDm34=', 'LHg+S8OtTw==', 'wqhOw715dsOH', 'U8O7VsO0wqvDvcKuKsOqX8Kr', 'Yittw5DDnWnDrA==', 'YMKIwqUUfgIk', 'aB7DlMODTQ==', 'wpfDh8Orw6kk', 'w7vCqMOrY8KAVk5OwpnCu8OaXsKZP3DClcKyw6HDrQ==', 'wow+w6vDmHpsw7Rtwo98LC7CiG7CksORT8KlW8O5wr3Di8OTHsODeHjDmcKlJsKqVA==', 'NwV+', 'w7HDrcKtwpJawpZb', 'wpQswqvDiHpuw6I=', 'YMKUwqMJZQ==', 'KH1VKcOqKsK1', 'fQ5sFUkkwpI=', 'wrvCrcOBR8Kk', 'M3w0fQ==', 'w6xXwqPDvMOFwo5d']
+
+# for i,j in enumerate(_0x4818):
+#     print(i,j)
+
+
+
+def _0x55f3(a,b):
+    _0x4c97f0 = int(a,16)
+    print(_0x4c97f0)
+    _0x48181e = _0x4818[_0x4c97f0]
+    print(_0x48181e)
+    pass
+
+# 调用_0x55f3
+_0x55f3('0x19', 'Pg54')
+
